fdad99bbc5168bf9f88e2494f1d2d49cc019e348
Jrius Kaxibwe <30425075+Jrius4@users.noreply.github.com>
2019-05-03 15:06:42 +0300

    UPDATE LOGIN

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 
 <div class="login-box">
   <div class="login-logo">
-    <a href="/"><b>NDEBITECH</b>APP</a>
+    <a href="/"><b>JUNIORDESIGN</b>APP</a>
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
