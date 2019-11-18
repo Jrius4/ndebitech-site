9fb8641cc9f89daf9450d6cd9df35025d9662b27
Kazibwe Julius Junior <kazibwejuliusjunior@gmail.com>
2019-11-18 19:04:04 +0300

    Integrate UI

--- resources/views/backend/career-categories/create.blade.php
+++ resources/views/backend/career-categories/create.blade.php
@@ -4,20 +4,20 @@
 
 @section('content')
 
-    
+
        <!-- Content Header (Page header) -->
        <section class="content-header">
           <div class="container-fluid">
             <div class="row mb-2">
               <div class="col-sm-6">
                 <h1><h1>
-                    Categories
-                    <small>Create New category</small></h1>
+                    Career Categories
+                    <small>Create New  category</small></h1>
               </div>
               <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
                   <li class="breadcrumb-item"><a href="{{ url('/home') }}"><i class="nav-icon fas fa-tachometer-alt"></i> Dashboard</a></li>
-                  <li class="breadcrumb-item"><a href="{{ route('backend.service-categories.index') }}">Service Categories</a></li>
+                  <li class="breadcrumb-item"><a href="{{ route('backend.career-categories.index') }}">Service Categories</a></li>
                   <li class="breadcrumb-item active">Create New Category</li>
                 </ol>
               </div>
@@ -32,19 +32,19 @@
                 <div class="col-lg-12">
                   <div class="card card-primary shadow-sm p-2">
                     <div class="card-title">
-                      <h3>Create Category</h3>
+                      <h3>Create Career Category</h3>
                     </div>
                   </div>
               {!! Form::model($category, [
                   'method' => 'POST',
-                  'route'  => 'backend.service-categories.store',
+                  'route'  => 'backend.career-categories.store',
                   'files'  => TRUE,
                   'id' => 'category-form'
               ]) !!}
 
               <div class="row d-flex justify-content-between">
-                @include('backend.service-categories.form')
-              </div> 
+                @include('backend.career-categories.form')
+              </div>
 
             {!! Form::close() !!}
           </div>
@@ -54,8 +54,8 @@
     </div>
   </section>
       <!-- /.content -->
-  
+
 
 @endsection
 
-@include('backend.service-categories.script')
+@include('backend.career-categories.script')
