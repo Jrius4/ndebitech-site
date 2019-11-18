7d5c3fd39e3bb7f9c2476802648ddef7f39a8125
Kazibwe Julius Junior <kazibwejuliusjunior@gmail.com>
2019-11-18 18:33:42 +0300

    Integrate UI

--- resources/views/layouts/home/navbar.blade.php
+++ resources/views/layouts/home/navbar.blade.php
@@ -22,7 +22,7 @@
 
 
 
-          <nav class="navbar navbar-expand-lg px-lg-0 bg-primary navbar-dark">
+          <nav class="navbar navbar-expand-lg px-lg-0 bg-light navbar-light">
             <div class="container position-relative">
               {{-- <!-- Navbar Brand--> --}}
               <a href="index-2.html" class="navbar-brand"> <img width="35" height="35" src="{{asset('/img/logos/ndebi-tech-favi-blue.png')}}" alt="logo"></a>
@@ -31,94 +31,13 @@
               {{-- <!-- Navbar Menu--> --}}
               <div id="navbarSupportedContent" class="collapse navbar-collapse">
                 <div class="navbar-nav ml-auto align-items-lg-center">
-                  <div class="nav-item dropdown active"><a id="navbarHome" data-target="#" href="index-2.html" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link">Home<i class="fa fa-caret-down"></i></a>
-                    <div aria-labelledby="navbarHome" class="dropdown-menu"><a href="index-2.html" class="dropdown-item">Homepage 1</a><a href="index2.html" class="dropdown-item">Homepage 2</a></div>
-                  </div>
-                  <div class="nav-item"><a href="{{url('/processes')}}" class="nav-link">Processes</a></div>
-                <div class="nav-item"><a href="{{url('/careers')}}" class="nav-link">Careers</a></div>
-                  <div class="nav-item dropdown"><a id="navbarCategory" data-target="#" href="index-2.html" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link">Category<i class="fa fa-caret-down"></i></a>
-                    <div aria-labelledby="navbarCategory" class="dropdown-menu"><a href="category.html" class="dropdown-item">Full width</a><a href="category-left.html" class="dropdown-item">Left sidebar</a><a href="category-right.html" class="dropdown-item">Right sidebar</a></div>
-                  </div>
-                  {{-- <!-- Megamenu--> --}}
-                  <div class="nav-item"><a href="#" data-toggle="dropdown" class="nav-link">Template content<i class="fa fa-caret-down"></i></a>
-                    <div class="dropdown-menu megamenu">
-                      <div class="row">
-                        <div class="col-lg-4 d-none d-lg-block">
-                          <div style="background: center center url('https://d19m59y37dris4.cloudfront.net/photo/1-4-1/img/hero-bg-1.jpg') no-repeat;" class="w-100 h-100"></div>
-                        </div>
-                        <div class="col-lg-8">
-                          <div class="row px-lg-3 py-lg-5">
-                            <div class="col-lg-4">
-                                  <!-- Megamenu list-->
-                                  <h6 class="heading-line">Homepage</h6>
-                                  <ul class="megamenu-list list-unstyled">
-                                    <li class="megamenu-list-item"><a href="index-2.html" class="megamenu-list-link">Homepage 1   </a></li>
-                                    <li class="megamenu-list-item"><a href="index2.html" class="megamenu-list-link">Homepage 2   </a></li>
-                                  </ul>
-                                  {{-- <!-- Megamenu list--> --}}
-                                  <h6 class="heading-line">Shop</h6>
-                                  <ul class="megamenu-list list-unstyled">
-                                    <li class="megamenu-list-item"><a href="category.html" class="megamenu-list-link">Category - full width   </a></li>
-                                    <li class="megamenu-list-item"><a href="category-right.html" class="megamenu-list-link">Category - right sidebar   </a></li>
-                                    <li class="megamenu-list-item"><a href="category-left.html" class="megamenu-list-link">Category - left sidebar   </a></li>
-                                    <li class="megamenu-list-item"><a href="detail.html" class="megamenu-list-link">Product detail   </a></li>
-                                  </ul>
-                            </div>
-                            <div class="col-lg-4">
-                                  {{-- <!-- Megamenu list--> --}}
-                                  <h6 class="heading-line">Order process</h6>
-                                  <ul class="megamenu-list list-unstyled">
-                                    <li class="megamenu-list-item"><a href="cart.html" class="megamenu-list-link">Shopping cart + checkout   </a></li>
-                                  </ul>
-                                  <!-- Megamenu list-->
-                                  <h6 class="heading-line">Blog</h6>
-                                  <ul class="megamenu-list list-unstyled">
-                                    <li class="megamenu-list-item"><a href="blog.html" class="megamenu-list-link">Blog   </a></li>
-                                    <li class="megamenu-list-item"><a href="post.html" class="megamenu-list-link">Post   </a></li>
-                                  </ul>
-                                  {{-- <!-- Megamenu list--> --}}
-                                  <h6 class="heading-line">Pages</h6>
-                                  <ul class="megamenu-list list-unstyled">
-                                    <li class="megamenu-list-item"><a href="text.html" class="megamenu-list-link">Text page   </a></li>
-                                    <li class="megamenu-list-item"><a href="contact.html" class="megamenu-list-link">Contact   </a></li>
-                                  </ul>
-                            </div>
-                            <div class="col-lg-4">
-                                  {{-- <!-- Megamenu list--> --}}
-                                  <h6 class="heading-line">Some more links</h6>
-                                  <ul class="megamenu-list list-unstyled">
-                                    <li class="megamenu-list-item"><a href="#" class="megamenu-list-link">Lorem   </a></li>
-                                    <li class="megamenu-list-item"><a href="##" class="megamenu-list-link">Ipsum dolor   </a></li>
-                                    <li class="megamenu-list-item"><a href="###" class="megamenu-list-link">Sit amet   </a></li>
-                                    <li class="megamenu-list-item"><a href="####" class="megamenu-list-link">Lorem   </a></li>
-                                    <li class="megamenu-list-item"><a href="#####" class="megamenu-list-link">Ipsum dolor   </a></li>
-                                    <li class="megamenu-list-item"><a href="######" class="megamenu-list-link">Sit amet   </a></li>
-                                  </ul>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  {{-- <!-- /Megamenu end    -->
-                  <!-- Multi level dropdown    --> --}}
-                  <div class="nav-item dropdown"><a id="navbarDropdownMenuLink" href="http://example.com/" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link">Dropdown<i class="fa fa-caret-down"></i></a>
-                    <div aria-labelledby="navbarDropdownMenuLink" class="dropdown-menu"><a href="#" class="dropdown-item">Action</a><a href="#" class="dropdown-item">Another action</a>
-                      <div class="dropdown-submenu"><a id="navbarDropdownMenuLink2" href="http://example.com/" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-item">Multilevel dropdown<i class="fa fa-caret-down"></i></a>
-                        <div aria-labelledby="navbarDropdownMenuLink2" class="dropdown-menu"><a href="#" class="dropdown-item">Action</a>
-                          <div class="dropdown-submenu"><a id="navbarDropdownMenuLink3" href="http://example.com/" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-item">
 
-                              Another action<i class="fa fa-caret-down"></i></a>
-                            <div aria-labelledby="navbarDropdownMenuLink3" class="dropdown-menu"><a href="#" class="dropdown-item">Action</a><a href="#" class="dropdown-item">Action</a><a href="#" class="dropdown-item">Action</a><a href="#" class="dropdown-item">Action</a></div>
-                          </div><a href="#" class="dropdown-item">Something else here</a>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  {{-- <!-- Multi level dropdown end      --> --}}
+                  <div class="nav-item active"><a href="{{url('/')}}" class="nav-link">Home</a></div>
+                  <div class="nav-item"><a href="{{url('/processes')}}" class="nav-link">Processes</a></div>
+
                 <div class="nav-item"><a href="{{url('/blog')}}" class="nav-link">Blog</a></div>
                 <div class="nav-item"><a href="{{url('/careers')}}" class="nav-link">Careers</a></div>
-                  <div class="nav-item"><a href="contact.html" class="nav-link">Contact Us</a></div>
+                  <div class="nav-item"><a href="#" class="nav-link">Contact Us</a></div>
                   <div class="nav-item">
                     <ul class="list-inline">
                     <li class="list-inline-item">
