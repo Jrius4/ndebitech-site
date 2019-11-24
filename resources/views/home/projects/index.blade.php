f47b4bb28a968d6d3de9042a60bdc3d778a82e2e
Kazibwe Julius Junior <kazibwejuliusjunior@gmail.com>
2019-11-24 11:47:35 +0300

    seeding

--- resources/views/home/projects/index.blade.php
+++ resources/views/home/projects/index.blade.php
@@ -2,14 +2,12 @@
 
 @section('content')
     <header class="mt-0 pt-0" style="margin-top:-200px">
-    <div style="background:{{false?null:'radial-gradient(circle, rgba(28,41,223,0.8799719716988358) 0%, rgba(5,96,203,1) 100%);'}} min-height:250px;min-width:100%;margin-top:-20px">
+    <div style="background:{{false?null:'radial-gradient(circle, rgba(28,41,223,0.8799719716988358) 0%, rgba(5,96,203,1) 100%);'}} min-height:150px;min-width:100%;margin-top:-20px">
             <img src="" alt="">
-            <div class="row d-flex justify-content-center">
+            <div class="row d-flex text-center justify-content-center">
                     <div class=" col-md-6 text-light align-self-baseline">
-                            <h3>Blog</h3>
-                        <p>
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. A quasi temporibus explicabo nulla maxime, eveniet, sapiente quo maiores commodi natus totam consequatur esse quidem architecto? Pariatur at odit natus sint!
-                        </p>
+                            <h3 class="m-auto">Projects</h3>
+
 
                     </div>
             </div>
